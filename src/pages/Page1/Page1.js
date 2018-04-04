b1ae188e31803947c2cfd9135e2d30e7aa4eb58a
houxiaobiao <xiaobiao.hou@gmail.com>
2018-04-04 15:08:30 +0800

    redux css redux-thunk

--- src/pages/Page1/Page1.js
+++ src/pages/Page1/Page1.js
@@ -3,7 +3,7 @@ import React, {Component} from 'react'
 export default class Page1 extends Component {
     render() {
         return (
-            <div>
+            <div className="page-box">
                 this is page1~!
             </div>
         )
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -9,6 +9,10 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
+
+    /** 报错配置 */
+    devtool: 'inline-source-map',
+
     /* src文件夹下面的以.js结尾的文件，要用babel解析 */
     /* cacheDirectory是用来缓存编译结果，下次编译加速 */
     module: {
@@ -16,7 +20,21 @@ module.exports = {
             test: /\.js$/,
             use: ['babel-loader?cacheDirectory=true'],
             include: path.join(__dirname, 'src')
+        },{
+            test: /\.css$/,
+            use: ['style-loader', 'css-loader']
         }]
+    },
+    resolve: {
+        /* 别名配置 */
+        alias: {
+            pages: path.join(__dirname, 'src/pages'),
+            component: path.join(__dirname, 'src/component'),
+            router: path.join(__dirname, 'src/router'),
+            actions: path.join(__dirname, 'src/redux/actions'),
+            reducers: path.join(__dirname, 'src/redux/reducers'),
+            //redux: path.join(__dirname, 'src/redux')
+        }
     },
 
     // 本地开发环境静态服务器
